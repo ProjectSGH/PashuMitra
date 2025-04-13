b00db3f600d0a98ceee4363d5f867197701eac25
Dhruv <sheredhruv@gmail.com>
2025-04-13 22:27:23 +0530

    feat - Updating the Folder Stucture and Adding a Backend Environment of Project

---
+++ pashumitra_backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
