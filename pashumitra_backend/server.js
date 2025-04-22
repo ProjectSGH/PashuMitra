d294505c60c2ed260ce660e415306bcb97ff1072
Dhruv <sheredhruv@gmail.com>
2025-04-22 09:46:11 +0530

    Set up MongoDB connection and update server configuration; add .env file and adjust .gitignore

--- pashumitra_backend/server.js
+++ pashumitra_backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connectDB = require("./config/db");
 const cors = require('cors');
 const dotenv = require('dotenv');
 
@@ -11,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Sample route
+connectDB(); // Connect to MongoDB
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
